5164338071bc634a0e1f8a6e50e210cf6fa5649f
htrannnn <tran.htran6123@gmail.com>
2025-06-30 16:11:02 +0700

    done check in card

--- src/components/user/CheckComponent.js
+++ src/components/user/CheckComponent.js
@@ -13,13 +13,7 @@ function CheckComponent() {
 	useEffect(() => {
 		const fetchData = async () => {
 			const booking = await getBookingById(id);
-
-			const formattedData = {
-				...booking,
-				startDate: booking.startDate ? new Date(booking.startDate).toLocaleDateString("vi-VN") : "",
-				endDate: booking.endDate ? new Date(booking.endDate).toLocaleDateString("vi-VN") : "",
-			};
-			setCheckInBooking(formattedData);
+			setCheckInBooking(booking);
 
 			const [facilities] = await getAllFacilities();
 			setFacilityList(facilities);
@@ -47,7 +41,7 @@ function CheckComponent() {
 					{bookingRoom ? (
 						<Card className="d-flex flex-row align-items-center p-3 shadow-sm rounded-3 col-md-5" style={{ width: "60%" }}>
 							<div style={{ flex: "0 0 150px", marginRight: "20px" }}>
-								<img src={bookingRoom.image} alt={bookingRoom.name} style={{ height: "140px", objectFit: "cover", borderRadius: "8px" }} />
+								<img src={bookingRoom.image} alt={bookingRoom.name} style={{ height: "130px", objectFit: "cover", borderRadius: "8px" }} />
 							</div>
 							<div style={{ flex: 1 }}>
 								<h5 className="mb-1">{bookingRoom.name}</h5>
@@ -64,10 +58,32 @@ function CheckComponent() {
 						<p className="text-center text-muted">Loading room information...</p>
 					)}
 					<Card className="d-flex flex-row align-items-center p-3 shadow-sm rounded-3 col-md-5 ms-2" style={{ width: "38%" }}>
+						<div style={{ fontSize: "16px" }}>
+							<h5 className="mb-1">Guest Information</h5>
+							<p className="mb-1 text-muted">
+								Name: {checkInBooking?.customer.firstName} {checkInBooking?.customer.lastName}
+							</p>
+							<p className="mb-1 text-muted">Email: {checkInBooking?.customer.email}</p>
+							<p className="mb-1 text-muted">
+								Guest: {checkInBooking.adult} adult, {checkInBooking.children} children
+							</p>
+							<p className="mb-1 text-muted">Special request: {checkInBooking.note ? <span>{checkInBooking.note}</span> : "none"}</p>
+						</div>
+					</Card>
+				</div>
+
+				<div>
+					<Card className="d-flex flex-row align-items-center p-3 shadow-sm rounded-3 mt-2" style={{ marginLeft: "-10px" }}>
 						<div style={{ flex: 1, fontSize: "16px" }}>
 							<h5 className="mb-1">
-								{checkInBooking.startDate} - {checkInBooking.endDate}
+								<p className="d-inline">
+									{checkInBooking?.startDate && checkInBooking?.endDate
+										? `${Math.ceil((new Date(checkInBooking.endDate) - new Date(checkInBooking.startDate)) / (1000 * 60 * 60 * 24))} nights`
+										: "Nights: -"}
+								</p>
+								, {new Date(checkInBooking.startDate).toLocaleDateString("vi-VN")} - {new Date(checkInBooking.endDate).toLocaleDateString("vi-VN")}
 							</h5>
+
 							<p className="mb-1 text-muted">Check in: 02:00 PM </p>
 							<p className="mb-1 text-muted">Check out: 12:00 PM </p>
 							<p className="mb-1 text-muted">Address: 103 - 105 Vo Nguyen Giap Street, Khue My Ward, Ngu Hanh Son District, Danang City, Vietnam. </p>
@@ -75,17 +91,8 @@ function CheckComponent() {
 					</Card>
 				</div>
 
-				<div>
-					<Card className="d-flex flex-row align-items-center p-3 shadow-sm rounded-3 mt-2">
-						<div style={{ fontSize: "16px" }}>
-							<h5 className="mb-1">
-								{checkInBooking.startDate} - {checkInBooking.endDate}
-							</h5>
-							<p className="mb-1 text-muted">Check in: 02:00 PM </p>
-							<p className="mb-1 text-muted">Check out: 12:00 PM </p>
-							<p className="mb-1 text-muted">Address: 103 - 105 Vo Nguyen Giap Street, Khue My Ward, Ngu Hanh Son District, Danang City, Vietnam. </p>
-						</div>
-					</Card>
+				<div className="mt-2">
+					<h5 className="text-center">Please capture this so you can check in upon arrival at the hotel.</h5>
 				</div>
 			</div>
 		</div>
